c0335b21eafe350b6d2b34aa6f515c11233dbdb9
Devan Skeem <devanskeem@gmail.com>
2019-07-17 14:15:52 -0600

    first commit

---
+++ csharp-intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharpintro
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
